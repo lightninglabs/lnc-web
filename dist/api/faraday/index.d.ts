f1caee3e38a860bcfc0998f49b928079bb66634b
Evan Kaloudis <zeusln@tutanota.com>
2022-04-21 20:46:28 -0400

    remove load, connect, disconnect functions from api classes

--- dist/api/faraday/index.d.ts
+++ dist/api/faraday/index.d.ts
@@ -1,4 +1,3 @@
-import * as FARADAY from '../../types/generated/faraday_pb';
 import WasmClient from './../../index';
 import BaseApi from './../base';
 /** the names and argument types for the subscription events */
@@ -11,20 +10,5 @@ declare class FaradayApi extends BaseApi<FaradayEvents> {
     _wasm: WasmClient;
     faradayServer: any;
     constructor(wasm: WasmClient);
-    /**
-     * Downloads the WASM binary and initializes the executable
-     */
-    load(): Promise<void>;
-    /**
-     * Uses the WasmClient to connect to the backend LND node via a Terminal
-     * Connect proxy server
-     * @param server the proxy server to connect to
-     * @param phrase the secret pairing phrase to use
-     */
-    connect(server: string, phrase: string): Promise<FARADAY.RevenueReportResponse.AsObject>;
-    /**
-     * Disconnects from the proxy server
-     */
-    disconnect(): void;
 }
 export default FaradayApi;
