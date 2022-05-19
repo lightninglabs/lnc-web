03e23609fc2d7883621fbebe94eac760617ecfe4
Evan Kaloudis <zeusln@tutanota.com>
2022-05-19 01:52:25 -0400

    gitignore: do not track build (dist) folder in git

--- dist/api/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as GrpcClient } from './grpc';
-export { default as LndApi } from './lnd';
-export { default as LoopApi } from './loop';
-export { default as PoolApi } from './pool';
-export { default as FaradayApi } from './faraday';
