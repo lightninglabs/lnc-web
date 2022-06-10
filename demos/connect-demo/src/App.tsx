a3b95bb4d1731d4b786ef9d70bba385a6876d6d5
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2022-06-09 23:32:53 -0400

    demo+connect: add pages to connect to a node

--- demos/connect-demo/src/App.tsx
+++ demos/connect-demo/src/App.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
+import Connect from './pages/Connect';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" className="mb-3">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt="logo"
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            lnc-web demo
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="connect" element={<Connect />} />
+          <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
--- demos/connect-demo/src/pages/Home.tsx
+++ demos/connect-demo/src/pages/Home.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import GetInfo from '../components/GetInfo';
+import Page from '../components/Page';
+import useLNC from '../hooks/useLNC';
 
 const Home: React.FC = () => {
+  const { lnc } = useLNC();
+
   return (
-    <Container>
-      <Row>
-        <Col>Welcome to lnc-web</Col>
-      </Row>
-    </Container>
+    <Page>
+      <h2 className="text-center">Welcome to lnc-web</h2>
+      <p className="text-center">
+        {lnc.isConnected
+          ? 'You are now connected to your Lightning node.'
+          : 'Connect or Login to view your Lightning node info.'}
+      </p>
+      <GetInfo />
+    </Page>
   );
 };
 
