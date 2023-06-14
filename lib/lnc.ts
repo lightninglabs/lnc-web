076c11ae1fbb896a6d85e7fb0ae8d1238f18450f
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2023-06-14 14:50:11 -0400

    lib: bump LNC wasm file to v0.2.5-alpha

--- lib/lnc.ts
+++ lib/lnc.ts
@@ -13,7 +13,7 @@ import { wasmLog as log } from './util/log';
 
 /** The default values for the LncConfig options */
 const DEFAULT_CONFIG = {
-    wasmClientCode: 'https://lightning.engineering/lnc-v0.2.4-alpha.wasm',
+    wasmClientCode: 'https://lightning.engineering/lnc-v0.2.5-alpha.wasm',
     namespace: 'default',
     serverHost: 'mailbox.terminal.lightning.today:443'
 } as Required<LncConfig>;
