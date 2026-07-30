33005d0c24709250bbba106d3d399f4e1a944751
Viktor Torstensson <viktor.t.git@gmail.com>
2026-07-30 10:11:08 +0200

    lib: update wasm url to v0.4.0-alpha

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 /** The default URL for the LNC WASM client binary. */
 export const WASM_CLIENT_URL =
-  'https://lightning.engineering/lnc-v0.3.6-alpha.wasm';
+  'https://lightning.engineering/lnc-v0.4.0-alpha.wasm';
